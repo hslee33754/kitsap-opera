23d480874eb256f0d6541f6f1c5b4c9af76a846f
Kate Lee <hslee.33754@gmail.com>
2016-07-18 17:53:40 -0700

    add index.php basic template

---
+++ index.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>HTML5 bare-bones structure</title>
+    
+    <!-- Meta Tags -->
+    <meta name="description" content="summary of page content">
+    <meta name="keywords" content="keywords, keyword">
+    <meta name="author" content="author of the document">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Style Sheets -->
+    <link rel="stylesheet" href="style.css">
+    
+    <!-- HTML5 shiv -->
+    <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</head>
+<body>
+    
+<!-- start header-->
+<header>
+<nav>
+    <ul>
+        <li>Page1</li>
+        <li>Page2</li>
+    </ul>
+</nav>
+</header>
+<!-- end header-->
+
+<!-- start contnet -->
+<section>
+
+</section>
+<!-- end content-->
+
+<!-- start sidebar -->
+<aside>
+
+</aside>
+<!-- end sidebar-->
+
+<!-- start footer -->
+<footer>
+    
+</footer>
+<!-- end footer -->
+
+<!-- start cripts -->
+<script src="js/scripts.js"></script>
+<!-- end cripts -->
+
+</body> 
+</html>
