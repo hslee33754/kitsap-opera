b4fdbf2a0d60bb1d57d08ff425f6446340308a26
Kate Lee <hslee.33754@gmail.com>
2016-07-25 20:17:23 -0700

    Wireframe Prototype Theme

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Theme Name: Kitsap Opera
+Author: Kitsap Team (Adam Kozie, Kate Lee, Katherine Shaw)
+Author URI: https://github.com/hslee33754/kitsap-oper
+Description: WordPress theme for Kitsap Opera
+Version: 1.0
+*/
+
+//Register Navigation
+register_nav_menus(array(
+    //*menu location slugs (key) => descriptions (according value)
+    'main' => 'Main Nav',
+    'footer' => 'Footer Nav'
+));
+?>
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>HTML5 bare-bones structure</title>
+    <title><?php bloginfo('title'); ?></title>
     
     <!-- Meta Tags -->
     <meta name="description" content="summary of page content">
@@ -11,39 +11,103 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Style Sheets -->
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
     
     <!-- HTML5 shiv -->
     <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 </head>
 <body>
+<div class="container">
     
 <!-- start header-->
 <header>
-<nav>
-    <ul>
-        <li>Page1</li>
-        <li>Page2</li>
-    </ul>
-</nav>
+    <div class="row">
+        <div class="left_header">
+            <img class="logo" alt="logo" src="<?php bloginfo('template_directory')?>/img/logo.jpg">
+        </div>
+        <div class="right_header">
+            <a href="" target="_blank">Ticket</a> |
+            <a href="" target="_blank">Donate</a> |
+            <a href="" target="_blank">Special Event</a> |
+            <a href="" target="_blank">Facebook</a>
+        </div>
+    </div>
 </header>
+    
+<div class="row">
+    <nav><?php wp_nav_menu(array(
+        'theme_location' => 'main_menu'
+    )); ?></nav>
+</div>
 <!-- end header-->
 
-<!-- start contnet -->
+<!-- start carousel -->
 <section>
-
+    <div class="row">
+        <div class="carousel">
+            <!-- Find image attachments with tag carousel from the page--> 
+            <img class="logo" alt="logo" src="<?php bloginfo('template_directory')?>/img/carousel1.jpg">
+        </div>
+    </div>
+</section>
+<!-- end carousel -->
+    
+<!-- start contnet -->
+<section class="main_section">
+    <div class="row">
+        <!-- This contents should be pulled from the page -->
+        <h1>Welcome to Kitsap Opera</h1>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+    </div>
+    <div class="row">
+        <img class="main-cta" alt="ticket" src="<?php bloginfo('template_directory')?>/img/main_ticket.jpg">
+        <img class="main-cta" alt="donate" src="<?php bloginfo('template_directory')?>/img/main_donate.jpg">
+    </div>
+    <dlv class="row">
+        <img class="responsive_video" alt="video placeholder" src="<?php bloginfo('template_directory')?>/img/video.jpg">
+        <!-- find a video attachment from the page -->
+    </dlv>
 </section>
 <!-- end content-->
 
 <!-- start sidebar -->
 <aside>
-
+    <div class="row">
+        <h2>Calendar</h2>
+        <img class="side-cta" alt="widget" src="<?php bloginfo('template_directory')?>/img/calendar_widget.jpg">
+        <img class="side-cta" alt="widget" src="<?php bloginfo('template_directory')?>/img/calendar_widget.jpg">
+    </div>
+    <div class="row">
+        <img class="side-cta" alt="widget" src="<?php bloginfo('template_directory')?>/img/side_ticket.jpg">
+    </div>
+    <div class="row">
+        <h2>Contact Us</h2>
+        <address>
+            <class="tel">(360) 337-8199</class><br/>
+            P.O. Box 1071, Bremerton, WA 98337
+        </address>
+        <p><a href="#">Facebook</a></p>
+        <p><a href="#">Audition</a></p>
+    </div>
+    
 </aside>
 <!-- end sidebar-->
 
 <!-- start footer -->
 <footer>
-    
+    <div class="left_footer">
+        <img class="logo" alt="logo" src="<?php bloginfo('template_directory')?>/img/logo.jpg">
+        <p><small>Copyright &copy; <?php echo date('Y'); ?>, Kitsap Peninsula Opera Guild. ALL RIGHTS RESERVED.</small></p>
+    </div>
+    <div class="right_footer">
+        <address>
+            <class="tel">(360) 337-8199</class><br/>
+            P.O. Box 1071, Bremerton, WA 98337
+        </address>
+        <nav><?php wp_nav_menu(array(
+        'theme_location' => 'footer'
+    )); ?></nav>
+    </div>
 </footer>
 <!-- end footer -->
 
@@ -51,5 +115,6 @@
 <script src="js/scripts.js"></script>
 <!-- end cripts -->
 
+</div>
 </body> 
 </html>
