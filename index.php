7b6b0c5f4c7207a3e8038d55b0cbd77bfde26017
Kate Lee <hslee.33754@gmail.com>
2016-08-08 17:26:56 -0700

    Fix styles for a tags, add indexing on blog page

--- index.php
+++ index.php
@@ -5,7 +5,15 @@
     <div class="row">
         <!-- BEGIN CONTENTS -->
         <?php if(have_posts()) : while (have_posts()) : the_post();?>
-        <?php the_content('');?>
+        <article class="excerpt">
+            <a href="<?php the_permalink(); ?>" t><?php echo get_the_post_thumbnail($post->ID, 'thumbnail'); ?></a>
+            <h1><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h1>
+            <small>Posted on <?php the_time('F j, Y'); ?> in <?php the_category(', '); ?></small>
+            <p>
+                <?php echo strip_tags(get_the_excerpt()); ?>
+                <a href="<?php the_permalink(); ?>"> Read More &raquo;</a> 
+            </p>
+        </article>
         <?php endwhile; endif; ?>
     </div>
 </section>
