a4d3cbe5042845c67a458f12f1d514bd4157178a
atkozie <atkozie@gmail.com>
2016-08-11 10:43:34 -0700

    Styling loop links, widget
    title and read more styling from the loop, styling on calendar widget

--- index.php
+++ index.php
@@ -9,11 +9,11 @@
     <?php if(have_posts()) : while (have_posts()) : the_post();?>
         <article class="excerpt">
             <a href="<?php the_permalink(); ?>" t><?php echo get_the_post_thumbnail($post->ID, 'thumbnail'); ?></a>
-            <h1><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h1>
+            <h1><a class="blk" href="<?php the_permalink(); ?>"><?php the_title();?></a></h1>
             <small>Posted on <?php the_time('F j, Y'); ?> in <?php the_category(', '); ?></small>
             <p>
                 <?php echo strip_tags(get_the_excerpt()); ?>
-                <a href="<?php the_permalink(); ?>"> Read More &raquo;</a> 
+                <a class="blk read-more" href="<?php the_permalink(); ?>"> Read More &raquo;</a> 
             </p>
         </article>
     <?php endwhile; endif; ?>
