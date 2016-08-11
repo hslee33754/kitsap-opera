2fbb5d6d7b1f18f53ad11382a76c82f486e3ea50
Kate Lee <hslee.33754@gmail.com>
2016-08-10 17:40:26 -0700

    add page.php

---
+++ page.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+
+<!-- start contnet -->
+<section class="main_section">
+    <div class="row">
+
+<!-- BEGIN CONTENTS -->
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <h1><?php the_title(); ?></h1>
+    <?php the_content(''); ?>
+<?php endwhile; endif; ?>
+<small>page.php</small>
+
+    </div>
+</section>
+<!-- end content-->
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
